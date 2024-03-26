59b0c4099ebf7eedcac413d3759a9164394aaf2e
matiasFigliuolo <matias.figliuolo1@gmail.com>
2024-03-26 16:01:00 -0300

    Entrega Desafio 2 (lista de estudiantes)

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+interface Student {
+
+  Name: String;
+  Age: number;
+  Class: String;
+};
+
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
   styleUrl: './student.component.scss'
 })
 export class StudentComponent {
+
+  Students : Student [] = [
+
+    {Name: "Juan" , Age: 18 , Class: "First Grade"},
+    {Name: "Maria" , Age: 17 , Class: "First Grade"},
+    {Name: "Lucio" , Age: 19 , Class: "Second Grade"},
+    {Name: "Martin" , Age: 19 , Class: "Second Grade"},
+    {Name: "Lucas" , Age: 20 , Class: "Third Grade"},
+    {Name: "Matias" , Age: 17 , Class: "First Grade"}
+  ]
+  
 
 }
