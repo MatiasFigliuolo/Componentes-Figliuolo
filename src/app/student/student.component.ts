93e2f8d05c872dc580c2e754e787eb0ef98bd271
matiasFigliuolo <matias.figliuolo1@gmail.com>
2024-04-03 20:41:54 -0300

    Agregado validaciones (FALTA AUN POR PROBLEMAS TECNICOS EL AGREGADO DEL OBJETO AL ARRAY)

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 interface Student {
 
-  Name: String;
+  Name: string;
   Age: number;
-  Class: String;
+  Class: string;
 };
 
 @Component({
@@ -15,29 +15,50 @@ interface Student {
 })
 export class StudentComponent {
 
-  Students : Student [] = [
+  Students: Student[] = [
 
-    {Name: "Juan" , Age: 18 , Class: "First Grade"},
-    {Name: "Maria" , Age: 17 , Class: "First Grade"},
-    {Name: "Lucio" , Age: 19 , Class: "Second Grade"},
-    {Name: "Martin" , Age: 19 , Class: "Second Grade"},
-    {Name: "Lucas" , Age: 20 , Class: "Third Grade"},
-    {Name: "Matias" , Age: 17 , Class: "First Grade"}
+    { Name: "Juan", Age: 18, Class: "First Grade" },
+    { Name: "Maria", Age: 17, Class: "First Grade" },
+    { Name: "Lucio", Age: 19, Class: "Second Grade" },
+    { Name: "Martin", Age: 19, Class: "Second Grade" },
+    { Name: "Lucas", Age: 20, Class: "Third Grade" },
+    { Name: "Matias", Age: 17, Class: "First Grade" }
   ]
 
   showForm = false;
-  
-  showStudent () {
+
+  showStudent() {
     this.showForm = !this.showForm;
   }
 
- myForm = this.formbuilder.group({
-    name: this.formbuilder.control(''),
-    age: this.formbuilder.control(''),
-    Class: this.formbuilder.control(''),
+  myForm = this.formbuilder.group({
+    name: this.formbuilder.control('', Validators.required),
+    age: this.formbuilder.control('', Validators.required),
+    class: this.formbuilder.control('', Validators.required),
   });
-  constructor (private formbuilder : FormBuilder){}
+  constructor(private formbuilder: FormBuilder) { };
 
+  get nameControl() {
+
+    return this.myForm.get('name');
+
+  }
+
+  get ageControl() {
+
+    return this.myForm.get('age');
+
+  }
+
+
+  // addStudent() {
+  //     let newStudent: Student = {
+  //       Name: this.myForm.value.name,
+  //       age: this.myForm.value.age,
+  //       class: this.myForm.value.class
+  //     };
+  //     this.Students.push(newStudent);
+  //     this.myForm.reset();
+  //   }
+  // }
 }
-
-
